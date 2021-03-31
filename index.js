24b5b766dd0aec5c06093e44114324be2a0226db
daniel conroy <dcconroy3@gmail.com>
2021-03-31 04:00:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ function whisper(string) {
 }
 
 function logShout(string) {
-  console.log(string.toUpperCase()) 
+  console.log(string.toUpperCase())
 }
 
 function logWhisper(string) {
@@ -15,5 +15,5 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
-  
+
 }
